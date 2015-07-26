7e1712debebc6e3ee702c09a5c8a4b8c7202441a
Rob Simpson <rsimpson2@me.com>
2015-07-26 09:37:57 -0400

    authentication models app

---
+++ authentication/models.py
@@ -0,0 +1,29 @@
+from django.contrib.auth.models import AbstactBaseUser
+from django.db import models
+
+class Account(AbstactBaseUser):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=40, unique=True)
+
+    first_name = models.CharField(max_length=40, blank=True)
+    last_name = models.CharField(max_length=40, blank=True)
+    tagline = models.CharField(max_length=140, blank=True)
+
+    is_admin = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    objects = AccountManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __unicode__(self):
+        return self.email
+
+    def get_full_name(self):
+        return ' '.join([self.first_name, self.last_name])
+
+    def get_short_name(self):
+        return self.first_name
