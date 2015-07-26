f7aa696aab126026db84c5f3999a2409f16de0e5
Rob Simpson <rsimpson2@me.com>
2015-07-26 10:07:16 -0400

    authentication permissions

---
+++ authentication/permissions.py
@@ -0,0 +1,7 @@
+from rest_framework import permissions
+
+class IsAccountOwner(permissions.BasePermission):
+    def has_object_permission(self, request, view, account):
+        if request.user:
+            return account = request.user
+        return false
