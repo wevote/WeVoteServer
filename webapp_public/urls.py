4ce977d4d7843dbbff3af651687ee6826a8b61c9
Rob Simpson <rsimpson2@me.com>
2015-07-26 10:10:29 -0400

    api route include

--- webapp_public/urls.py
+++ webapp_public/urls.py
@@ -1,9 +1,17 @@
 from django.conf.urls import patterns, url
+from rest_framework_nested import routers
+
+from authentication.views import AccountViewSet
+
+router = routers.SimpleRouter()
+router.register(r'accounts', AccountViewSet)
 
 from webapp_public.views import IndexView
 
 urlpatterns = patterns(
     '',
+
+    url(r'^api/v1/' include(router.urls)),
 
     url('^.*$', IndexView.as_view(), name='index'),
 )
