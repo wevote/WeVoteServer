fa471097584d9e7a39ac8f016dbd43c06ce6b314
Rob Simpson <rsimpson2@me.com>
2015-07-26 10:39:22 -0400

    angular register route

---
+++ static/javascripts/webapp_public.routes.js
@@ -0,0 +1,17 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('webapp_public.routes')
+        .config(config);
+
+    config.$inject = ['$routeProvider'];
+
+    function config($routeProvider) {
+        $routeProvider.when('/register', {
+            controller: 'RegisterController',
+            controllerAs: 'vm',
+            templateUrl: '/static/templates/authentication/register.html'
+        }).otherwise('/');
+    }
+})();
