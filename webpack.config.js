07b5357c278f079511a9287bb3014664d17e2701
Rob Simpson <rsimpson2@me.com>
2015-09-19 18:45:44 -0400

    setup of webpack module bundler

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var definePlugin = new webpack.DefinePlugin({
+    __DEV__: JSON.stringify(JSON.parse(process.env.BUILD_DEV || 'true')),
+    __PRERELEASE__: JSON.stringify(JSON.parse(process.env.PRERELEASE || 'false'))
+});
+
+module.exports = {
+    entry: {
+        User: './main.js',
+        Organization: './organization.js'
+    },
+    ouput: {
+        path: './build',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: {
+            { test: /\.css$/, loader: 'style-loader!css-loader' },
+            { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192' },
+            { test: /\.js$/, loader: 'babel-loader' },
+            { test: /\.scss$/, loader: 'style!css!sass' }
+        }
+    },
+    plugins: [definePlugin]
+};
