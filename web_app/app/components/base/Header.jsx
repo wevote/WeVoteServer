bd192e9d8d83a3bb22a2fcdb7d04ffc25b7dc7c2
Rob Simpson <rsimpson2@me.com>
2015-10-17 13:16:31 -0400

    Avatar component with FB API

--- web_app/app/components/base/Header.jsx
+++ web_app/app/components/base/Header.jsx
@@ -1,15 +1,16 @@
-let React = require("react");
+import React from "react";
 
 require("stylesheets/bootstrap.3.3.1.min");
 
-let Header = React.createClass({
+class Header extends React.Component {
   render() {
     return (
-      <div className="navbar navbar-default">
-        Top navbar starter
-      </div>
+      <section className="navbar navbar-default">
+        <h2>{this.props.title}</h2>
+        <section>{this.props.children}</section>
+      </section>
     );
   }
-});
+};
 
 React.render(<Header />, document.getElementById('content'));
