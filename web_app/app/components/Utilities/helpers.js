dbb94e7bd1085136b211d3b2c11fd56017ae491d
Rob Simpson <rsimpson2@me.com>
2015-10-21 04:59:11 -0400

    convert code to ES6

--- web_app/app/components/Utilities/helpers.js
+++ web_app/app/components/Utilities/helpers.js
@@ -1,19 +1,19 @@
-var axios = require('axios');
+import axios from 'axios';
 
 function getUserProfile(username) {
-  return axios.get('http://localhost:8000/users/' + username + '/');
+  return axios.get(`http://localhost:8000/users/${username}/`);
 }
 
 var helpers = {
-    getUserInfo: function(username) {
+    getUserInfo(username) {
       // axios.all returns array of promises
       return axios.all([getUserProfile(username)])
-        .then(function(arr) {
+        .then((arr) => {
           return {
             username: arr[0].data
           }
-        })
+        });
     }
 };
 
-module.exports = helpers;
+export default helpers;
