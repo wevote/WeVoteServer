8125a2ef2951696b6f2b3c1d739c5877e9a7e27e
Rob Simpson <rsimpson2@me.com>
2015-10-24 18:33:08 -0400

    modify Utilities to es6 format

--- web_app/app/components/Utilities/Search.js
+++ web_app/app/components/Utilities/Search.js
@@ -1,25 +1,24 @@
-var React = require('react');
-var Router = require('react-router');
+import React from 'react';
+import Router from 'react-router';
 
-var Search = React.createClass({
-    mixins: [Router.Navigation],
-    propTypes: {
-      searchText = React.PropTypes.string.isRequired
-    },
-    handleSearch: function () {
-      var searchInput = this.refs.getDOMNode().value;
-      this.refs.search.getDOMNode().value = 'Search';
-      this.transitionTo('profile', {searcInput: searchInput});
-    }
-    render: function () {
-      return (
-        <div class="col-sm-12">
-          <div className="input-group">
-            <input type="text" className="form-control" ref="search" placeholder="Search" onBlur={this.handleSearch} />
-          </div>
-        </div>
-      );
-    }
+const Search = React.createClass({
+  propTypes: {
+    searchText: React.PropTypes.string.isRequired
+  },
+  mixins: [Router.Navigation],
+  handleSearch() {
+    const searchInput = this.refs.getDOMNode().value;
+    this.refs.search.getDOMNode().value = 'Search';
+    this.transitionTo('profile', {searcInput: searchInput});
+  },
+  render() {
+    return (<div className="col-sm-12">
+      <div className="input-group">
+        <input type="text" className="form-control" ref="search" placeholder="Search" onBlur={this.handleSearch} />
+      </div>
+    </div>
+  );
+  }
 });
 
 module.exports = Search;
--- web_app/app/components/Utilities/helpers.js
+++ web_app/app/components/Utilities/helpers.js
@@ -4,16 +4,16 @@ function getUserProfile(username) {
   return axios.get(`http://localhost:8000/users/${username}/`);
 }
 
-var helpers = {
-    getUserInfo(username) {
-      // axios.all returns array of promises
-      return axios.all([getUserProfile(username)])
-        .then((arr) => {
-          return {
-            username: arr[0].data
-          }
-        });
-    }
+const helpers = {
+  getUserInfo(username) {
+    // axios.all returns array of promises
+    return axios.all([getUserProfile(username)])
+      .then((arr) => {
+        return {
+          username: arr[0].data
+        };
+      });
+  }
 };
 
 export default helpers;
