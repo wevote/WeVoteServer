d390a29b4258219851672a05468efb3dbebf8de7
Rob Simpson <rsimpson2@me.com>
2015-10-24 18:26:51 -0400

    change to es6 code format

--- web_app/app/components/Main.js
+++ web_app/app/components/Main.js
@@ -1,22 +1,21 @@
-var React = require('react');
-var RouteHandler = require('react-router').RouterHandler;
-var Search = require('utilities/Search');
+import React from 'react';
+const RouteHandler = require('react-router').RouterHandler;
+import Search from 'utilities/Search';
 
-var Main = React.createClass({
-    render: function () {
-        return (
-            <div className="main-container">
-                <nav className="navbar navbar-default" role="navigation">
-                    <div className="col-sm-7 col-sm-offset-2" style={{marginTop: 15}}>
-                        <Search />
-                    </div>
-                </nav>
-                <div className="container">
-                    <RouteHandler />
-                </div>
+const Main = React.createClass({
+  render() {
+    return (<div className="main-container">
+        <nav className="navbar navbar-default" role="navigation">
+            <div className="col-sm-7 col-sm-offset-2" style={{marginTop: 15}}>
+                <Search />
             </div>
-        )
-    }
+        </nav>
+        <div className="container">
+            <RouteHandler />
+        </div>
+    </div>
+    );
+  }
 });
 
 module.exports = Main;
