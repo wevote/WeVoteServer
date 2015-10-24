fb7fd80ebb278f2174dc4ac920a42cb60ce7d2cb
Rob Simpson <rsimpson2@me.com>
2015-10-24 18:21:47 -0400

    es6 update

--- web_app/app/components/Profile.js
+++ web_app/app/components/Profile.js
@@ -1,45 +1,39 @@
-var React = require('react');
-var Router = require('react-router');
-var UserProfile = require('/Users/UserProfile');
-var helpers = require('./utilities/helpers');
+import React from 'react';
+import Router from 'react-router';
+import UserProfile from '/Users/UserProfile';
+import helpers from './utilities/helpers';
 
-var Profile = React.createClass({
-    mixins: [Router.State],
-    init: function () {
-      // any ajax calls that are redundant put in here
-    },
-    getInitialState: function () {
-        return (
-            name: {},
-            bio: {}
-        )
-    },
-    componentDidMount: function () {
+const Profile = React.createClass({
+  mixins: [Router.State],
+  getInitialState() {
+    return (
+      name: {},
+      //bio: {}
+    )
+  },
+  componentDidMount() {
+    this.init();
+  },
+  componentWillUnmount() {
+
+  },
+  init() {
+
+  },
+  componentwillReceiveProps() {
+      // route change?
       this.init();
-    },
-    componentWillUnmount: function () {
-
-    },
-    componentwillReceiveProps: function () {
-        // route change?
-        this.init();
-    },
-    render: function () {
-        var username = this.getParams().username;
-        return (
-            <div className="row">
-                <div className="col-md-4">
-                    User Profile Component -- {username}
-                </div>
-                <div className="col-md-4">
-                    Repos Component
-                </div>
-                <div className="col-md-4">
-                    Notes Compoent
-                </div>
-            </div>
-        )
-    }
+  },
+  render() {
+      const username = this.getParams().username;
+      return (
+          <div className="row">
+              <div className="col-md-8">
+                  User Profile Component -- {username}
+              </div>
+          </div>
+      )
+  }
 });
 
 module.exports = Profile;
