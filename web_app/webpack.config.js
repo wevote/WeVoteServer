e2693f6371d55d1d6b748aeb9898ed938b14dfe2
Rob Simpson <rsimpson2@me.com>
2015-10-26 04:05:22 -0400

    setup of webpack

--- web_app/webpack.config.js
+++ web_app/webpack.config.js
@@ -1,15 +1,38 @@
 module.exports = {
-    entry: './app/App.js',
-    output: {
-        filename: 'public/bundle.js'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.jsx?$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: 'babel'
-            }
-        ]
-    }
+  entry: './app/App.js',
+  contentBase: './build',
+  output: {
+    filename: '[name]/bundle.js',
+    publicPath: '/',
+    path: path.resolve(__dirname, 'build')
+  },
+  module: {
+    preLoaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'eslint',
+        exclude: /node_modules|build/
+      }
+    ],
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loaders: ENV === 'development'
+          ? ['react-hot', 'babel']
+          : ['babel']
+        exclude: /(node_modules|build)/,
+      }
+    ]
+  },
+  resolve: {
+    root: [path.resolve('./app')],
+    extensions: ['', '.js', '.jsx']
+  },
+  plugins: ENV === 'development'
+    ? [new webpack.HotModuleReplacementPlugin()]
+    : [],
+  eslint: {configFile: '.eslintrc'},
+  node: {
+    fs: 'empty'
+  }
 };
