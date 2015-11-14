dc43779a59f576380c5257f51dc08f666aad9672
Dale John McGrew <dale.mcgrew@wevoteusa.org>
2015-11-14 09:41:48 -0800

    Refactored organizationRetrieve and organizationSave internal method names, and moved some code to match emerging standard.

--- web_app/app/components/base/Footer.jsx
+++ web_app/app/components/base/Footer.jsx
@@ -1,27 +1,37 @@
 import React from "react";
 import Router from "react-router";
 
+var Link = Router.Link;
+
 export default class Footer extends React.Component {
   render() {
     return <div className="row">
         <div className="navbar navbar-default navbar-fixed-bottom">
             <div className="container-fluid container-top10 seperator-top">
+                <Link to="ballot">
+                    <div className="col-xs-6 col-sm-3 center-block text-center">
+                      <span className="glyphicon glyphicon-list-alt"></span><br />
+                      <span className="text-center">My Ballot</span>
+                    </div>
+                </Link>
                 <div className="col-xs-6 col-sm-3 center-block text-center">
-                  <span className="glyphicon glyphicon-list-alt"></span><br />
-                  <span className="text-center">My Ballot</span>
+                  <span className="glyphicon glyphicon-question-sign"></span><br />
+                  <span className="text-center">Requests</span>
                 </div>
                 <div className="col-xs-6 col-sm-3 center-block text-center">
                   <span className="glyphicon glyphicon-question-sign"></span><br />
-                  <span className="text-center">Questions</span>
+                  <span className="text-center">Connect</span>
                 </div>
-                <div className="col-xs-6 col-sm-3 center-block text-center">
+                {/*<div className="col-xs-6 col-sm-3 center-block text-center">
                   <span className="glyphicon glyphicon-globe"></span><br />
-                  <span className="text-center">News Feed</span>
-                </div>
-                <div className="col-xs-6 col-sm-3 center-block text-center">
-                  <span className="glyphicon glyphicon-tasks"></span><br />
-                  <span className="text-center">More</span>
-                </div>
+                  <span className="text-center">Activity Feed</span>
+                </div>*/}
+                <Link to="more">
+                    <div className="col-xs-6 col-sm-3 center-block text-center">
+                      <span className="glyphicon glyphicon-tasks"></span><br />
+                      <span className="text-center">More</span>
+                    </div>
+                </Link>
             </div>
         </div>
     </div>
--- web_app/app/components/base/MainMenu.jsx
+++ web_app/app/components/base/MainMenu.jsx
@@ -8,8 +8,8 @@ export default class MainMenu extends React.Component {
             <nav className="navbar navbar-main navbar-fixed-top">
                 <div className="container-fluid">
                     <div className="left-inner-addon">
-                        <i className="glyphicon glyphicon-search icon-search"></i>
-                        <input type="text" className="form-control input-lg" placeholder="Search" />
+                        {/*<i className="glyphicon glyphicon-search icon-search"></i>*/}
+                        {/*<input type="text" className="form-control input-lg" placeholder="Search" />*/}
                 </div>
             </div>
       </nav>
