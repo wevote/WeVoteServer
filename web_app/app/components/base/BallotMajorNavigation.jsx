acfec36e42f1b0d91bf5afeaede726e6860c7b34
Rob Simpson <rsimpson2@me.com>
2015-11-21 15:30:43 -0500

    UI: Footer nav with font icons

--- web_app/app/components/base/BallotMajorNavigation.jsx
+++ web_app/app/components/base/BallotMajorNavigation.jsx
@@ -6,30 +6,32 @@ export default class BallotMajorNavigation extends React.Component {
     return <div className="row">
         <div className="navbar navbar-default navbar-fixed-bottom">
             <div className="container-fluid container-top10 seperator-top">
+              <div class="row">
                 <Link to="ballot">
-                    <div className="col-xs-6 col-sm-3 center-block text-center">
+                    <div className="col-xs-3 center-block text-center">
                       <span className="glyphicon glyphicon-list-alt"></span><br />
                       <span className="text-center">My Ballot</span>
                     </div>
                 </Link>
-                <div className="col-xs-6 col-sm-3 center-block text-center">
+                <div className="col-xs-2 center-block text-center">
                   <span className="glyphicon glyphicon-inbox"></span><br />
                   <span className="text-center">Requests</span>
                 </div>
-                <div className="col-xs-6 col-sm-3 center-block text-center icon_connect">
-                  <span className="icon_connect"></span><br />
+                <div className="col-xs-2 center-block text-center">
+                  <span className="glyphicon icon-icon-connect-1-3"></span><br />
                   <span className="text-center">Connect</span>
                 </div>
-                <div className="col-xs-6 col-sm-3 center-block text-center">
-                  <span className="glyphicon glyphicon-fire"></span><br />
+                <div className="col-xs-3 center-block text-center">
+                  <span className="glyphicon icon-icon-activity-1-4"></span><br />
                   <span className="text-center">Activity Feed</span>
                 </div>
                 <Link to="more">
-                    <div className="col-xs-6 col-sm-3 center-block text-center">
+                    <div className="col-xs-2 center-block text-center">
                       <span className="glyphicon glyphicon-menu-hamburger"></span><br />
                       <span className="text-center">More</span>
                     </div>
                 </Link>
+              </div>
             </div>
         </div>
     </div>
