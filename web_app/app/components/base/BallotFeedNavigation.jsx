c2a894327c3e799277b92ca4defe977fa649a952
Rob Simpson <rsimpson2@me.com>
2015-11-21 13:05:12 -0500

    UI of the My Ballot main header

--- web_app/app/components/base/BallotFeedNavigation.jsx
+++ web_app/app/components/base/BallotFeedNavigation.jsx
@@ -1,20 +1,26 @@
+//TODO: Break apart the add friends and more opinions list
 import React from "react";
 import { Link } from "react-router";
 
 export default class BallotFeedNavigation extends React.Component {
 	render() {
 		return <div className="row">
-            <nav className="navbar navbar-main navbar-fixed-top">
+            <nav className="navbar navbar-main navbar-fixed-top bottom-separator">
                 <div className="container-fluid">
                     <div className="left-inner-addon">
-					<h2 className="text-left">My Ballot</h2>
-                    <p className="text-right">
-                        <Link to="more_change_location">Oakland, CA (change)</Link>&nbsp;&nbsp;&nbsp;
-                        <Link to="add_friends">Add Friends</Link>&nbsp;&nbsp;&nbsp;
-                        <Link to="ballot_opinions">More Opinions</Link></p>
+                      <h2 className="pull-left no-space bold">My Ballot</h2>
+                      <ul className="nav nav-pills pull-right">
+                        <li><Link to="more_change_location" className="font-lightest">Oakland, CA (change)</Link></li>
+                      </ul>
                     </div>
                 </div>
             </nav>
+            <div>
+              <ul>
+                <li><Link to="add_friends">Add Friends</Link></li>
+                <li><Link to="ballot_opinions">More Opinions</Link></li>
+              </ul>
+            </div>
 		</div>;
 	}
 }
