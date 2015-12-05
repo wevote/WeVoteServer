5895a22e2c8be2a58f3d229e8d6354a707cf6e0f
Rob Simpson <rsimpson2@me.com>
2015-12-05 13:57:28 -0500

    #67 - Remove whitespace from request view

--- web_app/app/containers/RequestsPage.jsx
+++ web_app/app/containers/RequestsPage.jsx
@@ -16,31 +16,34 @@ export default class RequestsPage extends React.Component {
 
 	render() {
 	    return (
-			<div>
-                <MainMenu />
-                <div className="container-fluid well well-90">
+			<section>
+        <div className="container-fluid well well-90">
 					<h4 className="text-left">Friend Requests</h4>
-                    <ul className="list-group">
-                        <li className="list-group-item">
-		                    <FollowOrIgnoreAction action_text={"Add Friend"} />
-							<i className="icon-icon-add-friends-2-1 icon-light icon-medium"></i>{/* TODO icon-person-placeholder */}
-							Janet Smith</li>
-                        <li className="list-group-item">
-		                    <FollowOrIgnoreAction action_text={"Add Friend"} />
-							<i className="icon-icon-add-friends-2-1 icon-light icon-medium"></i>{/* TODO icon-person-placeholder */}
-							Will Rogers</li>
-                        <li className="list-group-item">
-		                    <FollowOrIgnoreAction action_text={"Add Friend"} />
-							<i className="icon-icon-add-friends-2-1 icon-light icon-medium"></i>{/* TODO icon-person-placeholder */}
-							Andrea Moed</li>
-                        <li className="list-group-item">
-		                    <FollowOrIgnoreAction action_text={"Add Friend"} />
-							<i className="icon-icon-add-friends-2-1 icon-light icon-medium"></i>{/* TODO icon-person-placeholder */}
-							Amy Muller</li>
-                    </ul>
-                </div>
-                <BallotMajorNavigation />
-			</div>
+              <ul className="list-group">
+                <li className="list-group-item">
+		              <FollowOrIgnoreAction action_text={"Add Friend"} />
+							    <i className="icon-icon-add-friends-2-1 icon-light icon-medium"></i>{/* TODO icon-person-placeholder */}
+							         Janet Smith
+                </li>
+                <li className="list-group-item">
+		              <FollowOrIgnoreAction action_text={"Add Friend"} />
+							    <i className="icon-icon-add-friends-2-1 icon-light icon-medium"></i>{/* TODO icon-person-placeholder */}
+							          Will Rogers
+                </li>
+                <li className="list-group-item">
+		              <FollowOrIgnoreAction action_text={"Add Friend"} />
+							    <i className="icon-icon-add-friends-2-1 icon-light icon-medium"></i>{/* TODO icon-person-placeholder */}
+							           Andrea Moed
+                </li>
+                <li className="list-group-item">
+		              <FollowOrIgnoreAction action_text={"Add Friend"} />
+							    <i className="icon-icon-add-friends-2-1 icon-light icon-medium"></i>{/* TODO icon-person-placeholder */}
+							           Amy Muller
+                </li>
+              </ul>
+        </div>
+        <BallotMajorNavigation />
+			</section>
 		);
 	}
 }
