3c14fb92e14f4d9d1c838c5ff249b6f4ef46ea68
Rob Simpson <rsimpson2@me.com>
2015-12-05 13:23:01 -0500

    #69 Change elements and positioning of ballot header

--- web_app/app/components/navigation/BallotFeedNavigation.jsx
+++ web_app/app/components/navigation/BallotFeedNavigation.jsx
@@ -6,28 +6,34 @@ import { Link } from "react-router";
 export default class BallotFeedNavigation extends React.Component {
 	render() {
 		return (
-<div className="row">
+<header className="row">
     <nav className="navbar navbar-main navbar-fixed-top bottom-separator">
-        <div className="container-fluid">
-            <div className="left-inner-addon">
+        <section className="container-fluid">
               <h4 className="pull-left no-space bold">My Ballot</h4>
-              <ul className="nav nav-pills pull-right">
-                <li><Link to="more_change_location" className="font-lightest">Oakland, CA (change)</Link></li>
-              </ul>
-            </div>
-        </div>
+              <aside className="pull-right">
+                <Link to="more_change_location"
+                      className="font-lightest">Oakland, CA (change)
+                </Link>
+              </aside>
+        </section>
     </nav>
     <div className="container-fluid bg-light box-skinny bottom-separator">
         <div className="row">
             <div className="col-xs-6 col-md-6 text-center">
                 <i className="icon-icon-add-friends-2-1 icon-light icon-medium"></i>
-                <Link to="add_friends" className="font-darkest fluff-left-narrow">Add Friends</Link></div>
+                <Link to="add_friends"
+                      className="font-darkest fluff-left-narrow">Add Friends
+                </Link>
+            </div>
             <div className="col-xs-6 col-md-6 text-center">
                 <i className="icon-icon-more-opinions-2-2 icon-light icon-medium"></i>
-                <Link to="ballot_opinions" className="font-darkest fluff-left-narrow">More Opinions</Link></div>
+                <Link to="ballot_opinions"
+                      className="font-darkest fluff-left-narrow">More Opinions
+                </Link>
+            </div>
         </div>
     </div>
-</div>
+</header>
         );
 	}
 }
