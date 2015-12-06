e9d91c446d2710371a8c3dc19a7017ee74027a77
Nick Fiorini <nf071590@gmail.com>
2015-12-06 11:50:03 -0500

    removing old app files

--- web_app/app/route-handlers/GuidesVoterPersonalEmailPage.jsx
+++
@@ -1,4 +0,0 @@
-import { createContainer } from "items-store";
-import GuidesVoterPersonalEmailPage from "../containers/guides_voter/GuidesVoterPersonalEmailPage";
-
-export default createContainer(GuidesVoterPersonalEmailPage);
