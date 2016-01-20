058b913746d21981613080eb7688f8ae6f3fa7ba
Dale John McGrew <dale.mcgrew@wevoteusa.org>
2016-01-19 17:45:16 -0800

    Created import_data folder for geoip download files.

--- config/base.py
+++ config/base.py
@@ -315,6 +315,6 @@ LOG_FILE_LEVEL = lookup_logging_level(get_environment_variable("LOG_FILE_LEVEL")
 
 # Using conventions from django.contrib:
 # https://docs.djangoproject.com/en/1.8/ref/contrib/gis/geoip/#geoip-settings
-GEOIP_PATH = os.path.join(BASE_DIR, 'geoip', 'data')
+GEOIP_PATH = os.path.join(BASE_DIR, 'geoip', 'import_data')
 GEOIP_COUNTRY = 'GeoIP.dat'
 GEOIP_CITY = 'GeoLiteCity.dat'
