09e50e2c76f56c1ae0842d225e4b480d806584f9
Dale John McGrew <dale.mcgrew@wevoteusa.org>
2016-06-28 18:31:51 -0700

    First draft of changes needed to support sign in with Twitter. (With rough documentation.)

---
+++ apis_v1/documentation_source/twitter_sign_in_request_access_token_doc.py
@@ -0,0 +1,67 @@
+# apis_v1/documentation_source/twitter_sign_in_request_access_token_doc.py
+# Brought to you by We Vote. Be good.
+# -*- coding: UTF-8 -*-
+
+
+def twitter_sign_in_request_access_token_doc_template_values(url_root):
+    """
+    Show documentation about twitterSignInRequestAccessToken
+    """
+    required_query_parameter_list = [
+        {
+            'name':         'voter_device_id',
+            'value':        'string',  # boolean, integer, long, string
+            'description':  'An 88 character unique identifier linked to a voter record on the server',
+        },
+        {
+            'name':         'api_key',
+            'value':        'string (from post, cookie, or get (in that order))',  # boolean, integer, long, string
+            'description':  'The unique key provided to any organization using the WeVoteServer APIs',
+        },
+    ]
+    optional_query_parameter_list = [
+    ]
+
+    potential_status_codes_list = [
+        {
+            'code':         'VALID_VOTER_DEVICE_ID_MISSING',
+            'description':  'Cannot proceed. A valid voter_device_id parameter was not included.',
+        },
+        {
+            'code':         'VALID_VOTER_ID_MISSING',
+            'description':  'Cannot proceed. A valid voter_id was not found.',
+        },
+        # {
+        #     'code':         '',
+        #     'description':  '',
+        # },
+    ]
+
+    try_now_link_variables_dict = {
+        # 'organization_we_vote_id': 'wv85org1',
+    }
+
+    api_response = '{\n' \
+                   '  "status": string,\n' \
+                   '  "success": boolean,\n' \
+                   '  "voter_device_id": string (88 characters long),\n' \
+                   '  "twitter_redirect_url": string,\n' \
+                   '}'
+
+    template_values = {
+        'api_name': 'twitterSignInRequestAccessToken',
+        'api_slug': 'twitterSignInRequestAccessToken',
+        'api_introduction':
+            "",
+        'try_now_link': 'apis_v1:twitterSignInRequestAccessTokenView',
+        'try_now_link_variables_dict': try_now_link_variables_dict,
+        'url_root': url_root,
+        'get_or_post': 'GET',
+        'required_query_parameter_list': required_query_parameter_list,
+        'optional_query_parameter_list': optional_query_parameter_list,
+        'api_response': api_response,
+        'api_response_notes':
+            "",
+        'potential_status_codes_list': potential_status_codes_list,
+    }
+    return template_values
