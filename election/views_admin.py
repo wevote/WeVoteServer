4bb305062e932e6f74e31d813a761108b0c59194
Dale John McGrew <dalemcgrew@gmail.com>
2016-10-17 22:32:34 -0700

    Removed limit to number of polling places to process.

--- election/views_admin.py
+++ election/views_admin.py
@@ -77,7 +77,7 @@ def election_all_ballots_retrieve_view(request, election_local_id=0):
         polling_location_list = polling_location_list.filter(state__iexact=state)
         # We used to have a limit of 500 ballots to pull per election, but now retrieve all
         # Ordering by "location_name" creates a bit of (locational) random order
-        polling_location_list = polling_location_list.order_by('location_name')[:200]  # [:500]  TODO DALE Remove soon
+        polling_location_list = polling_location_list.order_by('location_name')  # [:500]  TODO DALE Remove soon
     except PollingLocation.DoesNotExist:
         messages.add_message(request, messages.INFO,
                              'Could not retrieve ballot data for the {election_name}. '
