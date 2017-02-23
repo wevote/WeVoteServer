7e05e12e091b5362d766b5cde4a4b8a7da255da5
jainanisha90 <jainanisha90@gmail.com>
2017-02-22 17:37:47 -0800

    Added a new interface for creation and caching of resized images for voters

--- image/views_admin.py
+++ image/views_admin.py
@@ -6,7 +6,7 @@ from admin_tools.views import redirect_to_sign_in_page
 from django.contrib.auth.decorators import login_required
 from django.contrib.messages import get_messages
 from django.shortcuts import render
-from image.controllers import cache_all_kind_of_images_locally_for_all_voter
+from .controllers import cache_all_kind_of_images_locally_for_all_voter, create_resized_images_for_all_voters
 from voter.models import fetch_voter_id_from_voter_device_link, voter_has_authority
 import wevote_functions.admin
 from wevote_functions.functions import convert_to_int, get_voter_api_device_id
@@ -34,3 +34,20 @@ def cache_images_locally_for_all_voters_view(request):
         'voter_id_signed_in':               voter_id
     }
     return render(request, 'image/cache_images_locally_for_all_voters.html', template_values)
+
+
+@login_required
+def create_resized_images_for_all_voters_view(request, voter_id):
+    authority_required = {'admin'}  # admin, verified_volunteer
+    if not voter_has_authority(request, authority_required):
+        return redirect_to_sign_in_page(request, authority_required)
+
+    voter_id = convert_to_int(voter_id)
+    messages_on_stage = get_messages(request)
+    create_resized_images_for_all_voters_results = create_resized_images_for_all_voters(voter_id)
+    template_values = {
+        'messages_on_stage':                    messages_on_stage,
+        'create_resized_images_for_all_voters': create_resized_images_for_all_voters_results,
+        'voter_id_signed_in':                   voter_id
+    }
+    return render(request, 'image/create_resized_images_for_all_voters.html', template_values)
