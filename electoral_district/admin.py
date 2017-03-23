55eedc9d8191c581ae061aa853e4aa83ba4db221
Dale John McGrew <dalemcgrew@gmail.com>
2017-03-22 17:40:26 -0700

    Update admin.py

--- electoral_district/admin.py
+++ electoral_district/admin.py
@@ -1,7 +1,5 @@
-# election/admin.py
+# electoral_district/admin.py
 # Brought to you by We Vote. Be good.
 # -*- coding: UTF-8 -*-
 
-from django.contrib import admin
 
-# Register your models here.
