bce795b3a3c41097b92052158198e8b9863a47eb
Dale John McGrew <dale.mcgrew@wevoteusa.org>
2017-09-14 09:33:13 -0700

    Made some tweaks to Issue Image upload code.

--- issue/controllers.py
+++ issue/controllers.py
@@ -176,12 +176,14 @@ def issue_retrieve_for_api(issue_id, issue_we_vote_id):  # issueRetrieve
             'we_vote_id':               issue.we_vote_id,
             'issue_name':               issue.issue_name,
             'issue_description':        issue.issue_description,
-            'issue_image_url':          issue.issue_image_url,
-            'issue_photo_url_large':    issue.we_vote_hosted_profile_image_url_large
-                if positive_value_exists(issue.we_vote_hosted_profile_image_url_large)
-                else issue.issue_photo_url(),
-            'issue_photo_url_medium':   issue.we_vote_hosted_profile_image_url_medium,
-            'issue_photo_url_tiny':     issue.we_vote_hosted_profile_image_url_tiny,
+            'issue_image_url':          issue.we_vote_hosted_image_url_medium
+            if positive_value_exists(issue.we_vote_hosted_image_url_medium)
+            else issue.issue_image_url,
+            'issue_photo_url_large':    issue.we_vote_hosted_image_url_large
+            if positive_value_exists(issue.we_vote_hosted_image_url_large)
+            else issue.issue_image_url,
+            'issue_photo_url_medium':   issue.we_vote_hosted_image_url_medium,
+            'issue_photo_url_tiny':     issue.we_vote_hosted_image_url_tiny,
         }
     else:
         json_data = {
@@ -261,7 +263,9 @@ def issues_retrieve_for_api(voter_device_id, sort_formula, voter_issues_only=Non
                 'issue_we_vote_id':         issue.we_vote_id,
                 'issue_name':               issue.issue_name,
                 'issue_description':        issue.issue_description,
-                'issue_image_url':          issue.issue_image_url,
+                'issue_image_url':          issue.we_vote_hosted_image_url_medium
+                if positive_value_exists(issue.we_vote_hosted_image_url_medium)
+                else issue.issue_image_url,
                 'issue_photo_url_large':    issue.we_vote_hosted_image_url_large,
                 'issue_photo_url_medium':   issue.we_vote_hosted_image_url_medium,
                 'issue_photo_url_tiny':     issue.we_vote_hosted_image_url_tiny,
@@ -335,7 +339,9 @@ def retrieve_issues_to_follow_for_api(voter_device_id, sort_formula):  # retriev
                 'issue_we_vote_id':             issue.we_vote_id,
                 'issue_name':                   issue.issue_name,
                 'issue_description':            issue.issue_description,
-                'issue_image_url':              issue.issue_image_url,
+                'issue_image_url':              issue.we_vote_hosted_image_url_medium
+                if positive_value_exists(issue.we_vote_hosted_image_url_medium)
+                else issue.issue_image_url,
                 'issue_photo_url_large':        issue.we_vote_hosted_image_url_large,
                 'issue_photo_url_medium':       issue.we_vote_hosted_image_url_medium,
                 'issue_photo_url_tiny':         issue.we_vote_hosted_image_url_tiny,
