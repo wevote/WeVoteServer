813e3f18cee921bc53add6c4cbe0c1d23e3e12a5
Dale John McGrew <dalemcgrew@gmail.com>
2017-10-24 10:10:11 -0700

    Update urls.py

--- google_custom_search/urls.py
+++ google_custom_search/urls.py
@@ -1,8 +1,7 @@
-# twitter/urls.py
+# google_custom_search/urls.py
 # Brought to you by We Vote. Be good.
 # -*- coding: UTF-8 -*-
 
-# See also import_export_twitter/urls.py
 from django.conf.urls import url
 
 from . import views_admin
