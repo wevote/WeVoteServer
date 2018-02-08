f788c42bf197f3137cc7ab9ba2985b0da075a35f
Dale John McGrew <dale.mcgrew@wevoteusa.org>
2018-02-07 21:45:48 -0800

    Added Issues to voterGuidesToFollowRetrieve.

--- apis_v1/documentation_source/voter_guides_to_follow_retrieve_doc.py
+++ apis_v1/documentation_source/voter_guides_to_follow_retrieve_doc.py
@@ -121,6 +121,7 @@ def voter_guides_to_follow_retrieve_doc_template_values(url_root):
                    'The list of ballot_item_we_vote_ids this organization has information about,\n' \
                    '     "ballot_item_we_vote_ids_this_org_opposes": ' \
                    'The list of ballot_item_we_vote_ids opposed by this organization,\n' \
+                   '     "issue_we_vote_ids_linked": The list of issue_we_vote_ids linked to this organization,\n' \
                    '     "is_support": boolean (Exists if looking at voter guides for one ballot_item),\n' \
                    '     "is_positive_rating": boolean (Exists if looking at voter guides for one ballot_item),\n' \
                    '     "is_support_or_positive_rating": boolean (Exists if looking at one ballot_item),\n' \
