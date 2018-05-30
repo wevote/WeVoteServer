8a81b7b7b804f9193eede1b8340934675ccd5fcd
jangsoo <hejang@berkeley.edu>
2018-05-30 16:03:16 -0700

    implemented ballotpedia api counter

--- import_export_ballotpedia/models.py
+++ import_export_ballotpedia/models.py
@@ -10,47 +10,68 @@ from django.db import models
 import wevote_functions.admin
 from wevote_functions.functions import convert_to_int, positive_value_exists
 
+
 class BallotpediaApiCounter(models.Model):
-    datetime_of_Action = models.DateTimeField(verbose_name='date of action', null=False, auto_now=False)
+    datetime_of_action = models.DateTimeField(verbose_name='date and time of action', null=False, auto_now=True)
     kind_of_action = models.CharField(verbose_name="kind of call to ballotpedia", max_length=50, null=True, blank=True)
+    google_civic_election_id = models.PositiveIntegerField(verbose_name="google civic election id", null=True)
     ballotpedia_election_id = models.PositiveIntegerField(verbose_name="ballotpedia election id", null=True)
+
+
+
+class BallotpediaApiCounterDailySummary(models.Model):
+    date_of_action = models.DateField(verbose_name='date of action', null=False, auto_now=False)
+    kind_of_action = models.CharField(verbose_name="kind of call to ballotpedia", max_length=50, null=True, blank=True)
+    google_civic_election_id = models.PositiveIntegerField(verbose_name="google civic election id", null=True)
+    ballotpedia_election_id = models.PositiveIntegerField(verbose_name="ballotpedia election id", null=True)
+
+
 
 class BallotpediaApiCounterWeeklySummary(models.Model):
     year_of_action = models.SmallIntegerField(verbose_name='year of action', null=False)
     week_of_action = models.SmallIntegerField(verbose_name='number of the week', null=False)
     kind_of_action = models.CharField(verbose_name="kind of call to ballotpedia", max_length=50, null=True, blank=True)
+    google_civic_election_id = models.PositiveIntegerField(verbose_name="google civic election id", null=True)
     ballotpedia_election_id = models.PositiveIntegerField(verbose_name="ballotpedia election id", null=True)
+
 
 class BallotpediaApiCounterMonthlySummary(models.Model):
     year_of_action = models.SmallIntegerField(verbose_name='year of action', null=False)
     month_of_action = models.SmallIntegerField(verbose_name='number of the month', null=False)
     kind_of_action = models.CharField(verbose_name="kind of call to ballotpedia", max_length=50, null=True, blank=True)
+    google_civic_election_id = models.PositiveIntegerField(verbose_name="google civic election id", null=True)
     ballotpedia_election_id = models.PositiveIntegerField(verbose_name="ballotpedia election id", null=True)
 
 
 # noinspection PyBroadException
 class BallotpediaApiCounterManager(models.Model):
-    def create_counter_entry(selfself, kind_of_action, ballotpedia_election_id=0):
+    def create_counter_entry(self, kind_of_action, google_civic_election_id=0, ballotpedia_election_id=0):
+        """
+        Create an entry that records that a call to the Ballotpedia Api was made.
+        """
         try:
-            ballotpedia_election_id = convert_to_int(ballotpedia_election_id)
+            google_civic_election_id = convert_to_int(google_civic_election_id)
 
+            # TODO: We need to work out the timezone questions
             BallotpediaApiCounter.objects.create(
                 kind_of_action=kind_of_action,
+                google_civic_election_id=google_civic_election_id,
                 ballotpedia_election_id=ballotpedia_election_id,
             )
             success = True
-            status = 'EnTRY_SAVED'
+            status = 'ENTRY_SAVED'
         except Exception:
             success = False
             status = 'SOME_ERROR'
 
         results = {
-            'success': success,
-            'status': status,
+            'success':                  success,
+            'status':                   status,
         }
         return results
 
-    def retrieve_daily_summaries(self, kind_of_action='', ballotpedia_election_id=0):
+    def retrieve_daily_summaries(self, kind_of_action='', google_civic_election_id=0, ballotpedia_election_id=0):
+        # Start with today and cycle backwards in time
         daily_summaries = []
         day_on_stage = date.today()  # TODO: We need to work out the timezone questions
         number_found = 0
@@ -59,13 +80,18 @@ class BallotpediaApiCounterManager(models.Model):
         attempt_count = 0
 
         try:
+            # Limit the number of times this runs to EITHER 1) 5 positive numbers
+            #  OR 2) 30 days in the past, whichever comes first
             while number_found <= days_to_display and attempt_count <= maximum_attempts:
                 attempt_count += 1
                 counter_queryset = BallotpediaApiCounter.objects.all()
                 if positive_value_exists(kind_of_action):
                     counter_queryset = counter_queryset.filter(kind_of_action=kind_of_action)
+                if positive_value_exists(google_civic_election_id):
+                    counter_queryset = counter_queryset.filter(google_civic_election_id=google_civic_election_id)
                 if positive_value_exists(ballotpedia_election_id):
                     counter_queryset = counter_queryset.filter(ballotpedia_election_id=ballotpedia_election_id)
+
 
                 # Find the number of these entries on that particular day
                 counter_queryset = counter_queryset.filter(datetime_of_action__contains=day_on_stage)
