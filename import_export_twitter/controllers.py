f4f3f298784c41f4cd9bd39ede00af6a50f08774
SailingSteve <steve@podell.com>
2018-07-23 15:38:53 -0700

    Twitter retrieve should save candidate website https://github.com/wevote/WeVoteServer/issues/819 In refresh_twitter_candidate_details_for_election If we have a twitter_url, but no candidate_twitter_handle, extract the handle and save it. Then if we have a candidate_twitter_handle, but no twitter_url, build a twitter_url and save it. Then if we have a twitter_url, but no candidate_url, save the twitter_url as a candidate_url

--- import_export_twitter/controllers.py
+++ import_export_twitter/controllers.py
@@ -596,6 +596,7 @@ def refresh_twitter_candidate_details_for_election(google_civic_election_id, sta
         candidate_list = candidates_results['candidate_list_objects']
 
         for candidate in candidate_list:
+            logger.info("refresh_twitter_candidate_details_for_election: " + candidate.candidate_name)
             # Extract twitter_handle from google_civic_election information
             if positive_value_exists(candidate.twitter_url) \
                     and not positive_value_exists(candidate.candidate_twitter_handle):
@@ -603,6 +604,17 @@ def refresh_twitter_candidate_details_for_election(google_civic_election_id, sta
                 candidate.candidate_twitter_handle = extract_twitter_handle_from_text_string(candidate.twitter_url)
                 candidate.save()
                 twitter_handles_added += 1
+            if positive_value_exists(candidate.candidate_twitter_handle) and not positive_value_exists(candidate.twitter_url):
+                candidate.twitter_url = 'https://twitter.com/' + candidate.candidate_twitter_handle
+                logger.info(
+                    'refresh_twitter_candidate_details_for_election, twitter_url set to ' + candidate.twitter_url)
+                candidate.save()
+            if positive_value_exists(candidate.twitter_url) and not positive_value_exists(candidate.candidate_url):
+                candidate.candidate_url = candidate.twitter_url
+                logger.info(
+                    'refresh_twitter_candidate_details_for_election, candidate_url set to ' + candidate.candidate_url)
+                candidate.save()
+
             if positive_value_exists(candidate.candidate_twitter_handle):
                 refresh_twitter_candidate_details(candidate)
                 profiles_refreshed_with_twitter_data += 1
