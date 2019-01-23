05afe345f4d2baec081ffe29c58bb63d58077767
stevepodell <steve@podell.com>
2019-01-22 20:22:29 -0800

    Put back in reference to "GEOLITE2_DATABASE_LOCATION", but gave it a default value if undefined.

--- geoip/management/commands/update_geoip_data.py
+++ geoip/management/commands/update_geoip_data.py
@@ -11,25 +11,6 @@ download_folder = settings.GEOIP_PATH
 
 
 class Command(BaseCommand):
-    help = 'Updates GeoIP data in {}'.format(download_folder)
-    base_url = 'http://www.maxmind.com/download/geoip/database/'
-    files = ['GeoLiteCity.dat.gz', 'GeoLiteCountry/GeoIP.dat.gz']
-
     def handle(self, *args, **options):
-        for path in self.files:
-            root, filepath = os.path.split(path)
-            dowloadpath = os.path.join(download_folder, filepath)
-            download_url = urllib.parse.urljoin(self.base_url, path)
-            self.stdout.write('Downloading {} to {}\n'.format(download_url, dowloadpath))
-            urllib.request.urlretrieve(download_url, dowloadpath)
-            outfilepath, ext = os.path.splitext(dowloadpath)
-            if ext != '.gz':
-                raise CommandError('Something went wrong while decompressing {}'.format(dowloadpath))
-            self.stdout.write('Extracting {} to {}\n'.format(dowloadpath, outfilepath))
-            with gzip.open(dowloadpath, 'rb') as infile, open(outfilepath, 'wb') as outfile:
-                outfile.writelines(infile)
-            self.stdout.write('Deleting {}\n'.format(dowloadpath))
-            os.remove(dowloadpath)
-            self.stdout.write('Done with {}\n'.format(path))
-
-            self.stdout.write('\nDownload the paid db from MaxMind for more precise results. Ask Dale for the credentials'.format(path))
+        self.stdout.write('\nIf you want to get an updated free database (GeoLite2-City.mmdb) download it from https://dev.maxmind.com/geoip/geoip2/geolite2/, unzip it, and overwrite the source controlled version')
+        self.stdout.write('\nDownload the paid db from MaxMind for more precise results. Ask Dale for the credentials')
