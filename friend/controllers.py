16bd15577b86b5635256b5c97652bbc2d33b182a
Dale John McGrew <dale.mcgrew@wevote.us>
2019-04-15 11:17:00 -0700

    Added "considered_left" and "considered_right" to issuesRetrieve API. Causing the polling location link to open in a new window.

--- friend/controllers.py
+++ friend/controllers.py
@@ -465,7 +465,7 @@ def send_to_one_friend(voter_device_id, sender_voter, send_now, sender_email_wit
         "sender_network_details":       sender_network_details,
         "recipient_name":               recipient_name,
         "recipient_voter_email":        recipient_voter_email,
-        "see_all_friend_requests_url":  WEB_APP_ROOT_URL + "/more/network",
+        "see_all_friend_requests_url":  WEB_APP_ROOT_URL + "/more/network",  # In WebApp 2019, redirects to /friends
         "confirm_friend_request_url":   WEB_APP_ROOT_URL + "/more/network/key/" + invitation_secret_key,
         "recipient_unsubscribe_url":    WEB_APP_ROOT_URL + "/unsubscribe?email_key=1234",  # TODO Implement this
         "email_open_url":               WE_VOTE_SERVER_ROOT_URL + "/apis/v1/emailOpen?email_key=1234",
@@ -1155,7 +1155,7 @@ def friend_invitation_by_we_vote_id_send_for_api(voter_device_id, other_voter_we
                 "sender_network_details":       sender_network_details,
                 "recipient_name":               recipient_name,
                 "recipient_voter_email":        recipient_voter_email,
-                "see_all_friend_requests_url":  WEB_APP_ROOT_URL + "/more/network",
+                "see_all_friend_requests_url":  WEB_APP_ROOT_URL + "/more/network",  # In WebApp 2019, -> /friends
                 "confirm_friend_request_url":   WEB_APP_ROOT_URL + "/more/network/key/" + invitation_secret_key,
                 "recipient_unsubscribe_url":    WEB_APP_ROOT_URL + "/unsubscribe?email_key=1234",
                 "email_open_url":               WE_VOTE_SERVER_ROOT_URL + "/apis/v1/emailOpen?email_key=1234",
