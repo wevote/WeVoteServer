240bb7a266cf40e9b3bbac69033436905747a972
Dale John McGrew <dale.mcgrew@wevote.us>
2019-09-27 05:31:32 -0700

    Added code to move donations from one voter/organization to another during sign in merge.

--- wevote_functions/functions.py
+++ wevote_functions/functions.py
@@ -984,7 +984,13 @@ def extract_website_from_url(url_string):
     url_string = url_string.replace("https://", "")
     url_string = url_string.replace("http://", "")
     url_string = url_string.replace("www://", "")
-    if 'facebook' not in url_string and 'twitter' not in url_string:
+    if 'actblue' not in url_string \
+            and 'facebook' not in url_string \
+            and 'instagram' not in url_string \
+            and 'linkedin' not in url_string \
+            and 'nationbuilder' not in url_string \
+            and 'twitter' not in url_string \
+            and 'wikipedia' not in url_string:
         url_string = url_string.split("/")[0]
     return url_string
 
