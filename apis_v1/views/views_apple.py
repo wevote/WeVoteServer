52d12110d7064e81c6d1d71363fad682a4fdf3e8
stevepodell <steve@podell.com>
2020-08-07 13:49:04 -0700

    Fix to Sign in with Apple redirect after decrypting the JWT

--- apis_v1/views/views_apple.py
+++ apis_v1/views/views_apple.py
@@ -2,8 +2,8 @@
 # Brought to you by We Vote. Be good.
 # -*- coding: UTF-8 -*-
 from datetime import date, datetime
-from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse, HttpResponseRedirect
+from django.views.decorators.csrf import csrf_exempt
 from exception.models import print_to_log, handle_exception
 import json
 from apple.AppleResolver import AppleResolver
@@ -121,8 +121,6 @@ def sign_in_with_apple_for_api(voter_device_id, user_code, email, first_name,
     }
     return results
 
-# https://medium.com/@aamishbaloch/sign-in-with-apple-in-your-django-python-backend-b501daa835a9
-# https://github.com/pastre/backend-challenge-python/blob/f387a94cafbec8c404d2d9e0eb05a9cc0eefd208/reflections/views.py
 @csrf_exempt
 def sign_in_with_apple_oauth_redirect_view(request):  # appleSignInOauthRedirectDestination
     # These are going to the error log so that I can see them on Splunk  (for now)
@@ -135,8 +133,10 @@ def sign_in_with_apple_oauth_redirect_view(request):  # appleSignInOauthRedirect
         logger.error('appleSignInOauthRedirectDestination WRONG Method: ' + request.method)
 
     access_token = request.POST['id_token']
-    voter_device_id = request.POST['state']
-    print('id_token renamed as access_token: ', access_token)
+    state_dict = json.loads(request.POST['state'])
+    voter_device_id = state_dict['voter_device_id']
+    return_url = state_dict['return_url']
+    # print('id_token renamed as access_token: ', access_token)
 
     first_name = ''
     middle_name = ''
@@ -144,6 +144,9 @@ def sign_in_with_apple_oauth_redirect_view(request):  # appleSignInOauthRedirect
     email = ''
     user_code = ''
     if 'user' in request.POST:
+        # First time
+        # <QueryDict: {'state': ['steve'], 'code': ['cae6bae43fae8490d8c69b99950db4e7c.0.nruqx.ovW0z46777pdzB6777qwRA'],
+        #              'id_token': ['eyJraW...1P9S_vgWJHn4SMNjrujasVxWN7JSdDLuxrXN-6CCQ22Iqeg']}>
         user = request.POST['user']
         first_name = user['name']['firstName']
         if 'middle_name' in user['name']:
@@ -157,37 +160,14 @@ def sign_in_with_apple_oauth_redirect_view(request):  # appleSignInOauthRedirect
         email = results['email']
 
     voter_we_vote_id = fetch_voter_we_vote_id_from_voter_device_link(voter_device_id)
+    if not positive_value_exists(voter_we_vote_id):
+        logger.error('did not receive a voter_we_vote_id from voter_device_id in sign_in_with_apple_oauth_redirect_view ')
 
     sign_in_with_apple_for_api(voter_device_id, user_code, email, first_name,
                                middle_name, last_name, 'Web WeVoteWebApp', 'n/a',
                                'n/a', voter_we_vote_id)
 
-    return HttpResponseRedirect('https://localhost:3000/ballot')
-
-# <QueryDict: {'state': ['steve'], 'code': ['cae6bae43fae8490d8c69b99950db4e7c.0.nruqx.ovW0z46HMKpdzB69RNqwRA'], 'id_token': ['eyJraWQiOiJlWGF1bm1MIiwiYWxnIjoiUlMyNTYifQ.eyJpc3MiOiJodHRwczovL2FwcGxlaWQuYXBwbGUuY29tIiwiYXVkIjoidXMud2V2b3RlLndlYmFwcCIsImV4cCI6MTU5NjY0NDQ3NCwiaWF0IjoxNTk2NjQzODc0LCJzdWIiOiIwMDE0MDcuODcxMGMxZmMwYmY4NDMyOGJjYzg1YWMxY2EyNWU5YWEuMDQxNiIsImNfaGFzaCI6ImxjMXhjZ2NyY1RvRGp4YWFrYjZvYWciLCJlbWFpbCI6InN0ZXZlcG9kZWxsQHlhaG9vLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjoidHJ1ZSIsImF1dGhfdGltZSI6MTU5NjY0Mzg3NCwibm9uY2Vfc3VwcG9ydGVkIjp0cnVlfQ.EQK13mKJHq0J6qpertr8VQ95ORI-j8klxUH0aaBEpKWEJ2IMksaQgCqDQOsP1Bi0nJ-IFPZivgPju8axlRtXf0XCWF4h4HYqMy7xciSq6eNUK9Yej6Xb6BZFd5ITt76tfiFnzVfYMdL8mZSSlV9reED_rxODyWcbBcRzwHYFtfjY1AqEhC-ePKfeVorRAN8FMV1X_XYy1cVNshFulX3iofaVxvOpeS6CPuWA9K9DsJ2xBvcFMPXg15Tl2C8mFOraj6pQ862YlB41jkSF1sjQEAqRp4sshPIa96AjghG1P9S_vgWJHn4SMNjrujasVxWN7JSdDLuxrXN-6CCQ22Iqeg']}>
-
-
-
-# First TimeoutError[2020-08-04 15:57:48,909] [ERROR] Steves-MacBook-Pro-32GB-Oct-2109.local:apis_v1.views.views_apple: appleSignInOauthRedirectDestination dump GET: {}
-# [2020-08-04 15:57:48,911] [ERROR] Steves-MacBook-Pro-32GB-Oct-2109.local:apis_v1.views.views_apple: appleSignInOauthRedirectDestination dump POST: {"state": "steve", "code": "c67a2ea5e60ff4b03bcfb3ccb6fef8bd9.0.nruqx.hFm55XIyU6at0kc5aj6xSg", "id_token": "eyJraWQiOiI4NkQ4OEtmIiwiYWxnIjoiUlMyNTYifQ.eyJpc3MiOiJodHRwczovL2FwcGxlaWQuYXBwbGUuY29tIiwiYXVkIjoidXMud2V2b3RlLndlYmFwcCIsImV4cCI6MTU5NjU4MjQ2OCwiaWF0IjoxNTk2NTgxODY4LCJzdWIiOiIwMDE0MDcuODcxMGMxZmMwYmY4NDMyOGJjYzg1YWMxY2EyNWU5YWEuMDQxNiIsImNfaGFzaCI6IjVsOEU2aEQteUN2N0M2MmpSWWMzSmciLCJlbWFpbCI6InN0ZXZlcG9kZWxsQHlhaG9vLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjoidHJ1ZSIsImF1dGhfdGltZSI6MTU5NjU4MTg2OCwibm9uY2Vfc3VwcG9ydGVkIjp0cnVlfQ.Qvw3A1EloqQcM7f-4qa9D1zfiO5ospymHz9RPFJvP0mD3dEyJLUu5rJfOnJfcY52Pvzb-K9wYwkVd3kv_R7w1vY1MHDbEsX3gBhoCXcsIvfxEx8RVVnoo2cIsGrWy7JB_FBuJkSQMW3QuPqxmYzr4IOiULGj3tFV4vHmzbpPMOTz8VQkEGYOnpqtrj8sgsk47AMZypbU-cMgmlMnG5VjjT4LsZVFVIXF80gydX8tqXmAd7npJivgFs2xF8973UeT7M5t8jKQQf9yVWi_aTOFxMSd2e1n7lYnWPsUDdVPWgIu8DD714WBUze18sx3FkgvpBoKNtc9d84nDfh3qaKyZw", "user": "{\"name\":{\"firstName\":\"Steve\",\"lastName\":\"Podell\"},\"email\":\"stevepodell@yahoo.com\"}"}
-# [2020-08-04 15:57:48,911] [ERROR] Steves-MacBook-Pro-32GB-Oct-2109.local:apis_v1.views.views_apple: appleSignInOauthRedirectDestination dump body: state=steve&code=c67a2ea5e60ff4b03bcfb3ccb6fef8bd9.0.nruqx.hFm55XIyU6at0kc5aj6xSg&id_token=eyJraWQiOiI4NkQ4OEtmIiwiYWxnIjoiUlMyNTYifQ.eyJpc3MiOiJodHRwczovL2FwcGxlaWQuYXBwbGUuY29tIiwiYXVkIjoidXMud2V2b3RlLndlYmFwcCIsImV4cCI6MTU5NjU4MjQ2OCwiaWF0IjoxNTk2NTgxODY4LCJzdWIiOiIwMDE0MDcuODcxMGMxZmMwYmY4NDMyOGJjYzg1YWMxY2EyNWU5YWEuMDQxNiIsImNfaGFzaCI6IjVsOEU2aEQteUN2N0M2MmpSWWMzSmciLCJlbWFpbCI6InN0ZXZlcG9kZWxsQHlhaG9vLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjoidHJ1ZSIsImF1dGhfdGltZSI6MTU5NjU4MTg2OCwibm9uY2Vfc3VwcG9ydGVkIjp0cnVlfQ.Qvw3A1EloqQcM7f-4qa9D1zfiO5ospymHz9RPFJvP0mD3dEyJLUu5rJfOnJfcY52Pvzb-K9wYwkVd3kv_R7w1vY1MHDbEsX3gBhoCXcsIvfxEx8RVVnoo2cIsGrWy7JB_FBuJkSQMW3QuPqxmYzr4IOiULGj3tFV4vHmzbpPMOTz8VQkEGYOnpqtrj8sgsk47AMZypbU-cMgmlMnG5VjjT4LsZVFVIXF80gydX8tqXmAd7npJivgFs2xF8973UeT7M5t8jKQQf9yVWi_aTOFxMSd2e1n7lYnWPsUDdVPWgIu8DD714WBUze18sx3FkgvpBoKNtc9d84nDfh3qaKyZw&user=%7B%22name%22%3A%7B%22firstName%22%3A%22Steve%22%2C%22lastName%22%3A%22Podell%22%7D%2C%22email%22%3A%22stevepodell%40yahoo.com%22%7D
-# '{"name":{"firstName":"Steve","lastName":"Podell"},"email":"stevepodell@yahoo.com"}'
-# request.POST['user'] = '{"name":{"firstName":"Steve","lastName":"Podell"},"email":"stevepodell@yahoo.com"}'
-
-# <QueryDict: {'state': ['steve'], 'code': ['c4510d342610f400ab4cb78158da3251a.0.nruqx.dDDRUMMrcteqW-pY_XhEYA'], 'id_token': ['eyJraWQiOiI4NkQ4OEtmIiwiYWxnIjoiUlMyNTYifQ.eyJpc3MiOiJodHRwczovL2FwcGxlaWQuYXBwbGUuY29tIiwiYXVkIjoidXMud2V2b3RlLndlYmFwcCIsImV4cCI6MTU5NjU4Mjk4MiwiaWF0IjoxNTk2NTgyMzgyLCJzdWIiOiIwMDE0MDcuODcxMGMxZmMwYmY4NDMyOGJjYzg1YWMxY2EyNWU5YWEuMDQxNiIsImNfaGFzaCI6Im81c3BLRWdYN0ZNQUM3MEdXTVJDSEEiLCJlbWFpbCI6InN0ZXZlcG9kZWxsQHlhaG9vLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjoidHJ1ZSIsImF1dGhfdGltZSI6MTU5NjU4MjM4Miwibm9uY2Vfc3VwcG9ydGVkIjp0cnVlfQ.Y1JWnv71iwF3QvH0YpxGfH5igEkc01cTHZ8MaeBqHv1dQ66PRkS2ykkCX_oiIqtfCyqPB3UVR3oDiGo_K2czV_fdvnydJKiJPQeRExc5iiBjmHmZ4s_IZl_fDghHq0HQRRtWuxQ1JvV9CD1bf990J7uwBPm0Atmjbp2fQbaetF6MFChPa_EhXeptIEU0QQw3rtyXgltIRzz0bZOmL2MTPvzuOjPo89z0cVFMV4Y7HNNyPKbbdJeZ8LgwsO_4EQ9rOhJog2wQ-HBhV1raCBmoltflh7GamuNayBkBdbbr1Vt8EVMOx3tnhPxx1_uoLM3w-n-SMFDn0Qp4owbEL7-dOw']}>
-# eyJraWQiOiI4NkQ4OEtmIiwiYWxnIjoiUlMyNTYifQ.eyJpc3MiOiJodHRwczovL2FwcGxlaWQuYXBwbGUuY29tIiwiYXVkIjoidXMud2V2b3RlLndlYmFwcCIsImV4cCI6MTU5NjU4Mjk4MiwiaWF0IjoxNTk2NTgyMzgyLCJzdWIiOiIwMDE0MDcuODcxMGMxZmMwYmY4NDMyOGJjYzg1YWMxY2EyNWU5YWEuMDQxNiIsImNfaGFzaCI6Im81c3BLRWdYN0ZNQUM3MEdXTVJDSEEiLCJlbWFpbCI6InN0ZXZlcG9kZWxsQHlhaG9vLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjoidHJ1ZSIsImF1dGhfdGltZSI6MTU5NjU4MjM4Miwibm9uY2Vfc3VwcG9ydGVkIjp0cnVlfQ.Y1JWnv71iwF3QvH0YpxGfH5igEkc01cTHZ8MaeBqHv1dQ66PRkS2ykkCX_oiIqtfCyqPB3UVR3oDiGo_K2czV_fdvnydJKiJPQeRExc5iiBjmHmZ4s_IZl_fDghHq0HQRRtWuxQ1JvV9CD1bf990J7uwBPm0Atmjbp2fQbaetF6MFChPa_EhXeptIEU0QQw3rtyXgltIRzz0bZOmL2MTPvzuOjPo89z0cVFMV4Y7HNNyPKbbdJeZ8LgwsO_4EQ9rOhJog2wQ-HBhV1raCBmoltflh7GamuNayBkBdbbr1Vt8EVMOx3tnhPxx1_uoLM3w-n-SMFDn0Qp4owbEL7-dOw
-# verified_payload {'iss': 'https://appleid.apple.com', 'aud': 'us.wevote.webapp', 'exp': 1596643481, 'iat': 1596642881, 'sub': '001407.8710c1fc0bf84328bcc85ac1ca25e9aa.0416', 'c_hash': 'd2v3DNRwymB3aFaPgS8lXA', 'email': 'stevepodell@yahoo.com', 'email_verified': 'true', 'auth_time': 1596642881, 'nonce_supported': True}
-# verified_payload {
-#   'iss': 'https://appleid.apple.com',
-#   'aud': 'us.wevote.webapp',
-#   'exp': 1596643481,
-#   'iat': 1596642881,
-#   'sub': '001407.8710c1fc0bf84328bcc85ac1ca25e9aa.0416',
-#   'c_hash': 'd2v3DNRwymB3aFaPgS8lXA',
-#   'email': 'stevepodell@yahoo.com',
-#   'email_verified': 'true',
-#   'auth_time': 1596642881,
-#   'nonce_supported': True}
+    return HttpResponseRedirect(return_url)
 
 
 def validate_sign_in_with_apple_token(request):  # appleValidateSignInWithAppleToken
