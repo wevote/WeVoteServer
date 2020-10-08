b3904a129365b71aac9b62b54829ee5a6f1114e1
Alex Espinoza-Fuentes <alexespinoza11f@gmail.com>
2020-10-07 21:09:33 -0700

    setup the email retrieve test

---
+++ apis_v1/tests/test_views_voter_email_address_retrieve.py
@@ -0,0 +1,28 @@
+# apis_v1/test_views_voter_email_address_save.py
+# Brought to you by We Vote. Be good.
+# -*- coding: UTF-8 -*-
+
+from django.urls import reverse
+from django.test import TestCase
+from email_outbound.models import EmailAddress, EmailManager
+import json
+
+class WeVoteAPIsV1TestsVoterEmailAddressRetrieve(TestCase):
+    databases = ["default", "readonly"]
+
+    def setUp(self):
+        self.generate_voter_device_id_url = reverse("apis_v1:deviceIdGenerateView")
+        self.voter_create_url = reverse("apis_v1:voterCreateView")
+        self.voter_email_address_save_url = reverse("apis_v1:voterEmailAddressSaveView")
+        self.voter_email_address_retrieve_url = reverse("apis_v1:voterEmailAddressRetrieveView")
+
+    def test_retrieve_with_no_voter_device_id(self):
+        response = self.client.get(self.voter_email_address_retrieve_url)
+        json_data = json.loads(response.content.decode())
+
+
+
+
+
+
+
