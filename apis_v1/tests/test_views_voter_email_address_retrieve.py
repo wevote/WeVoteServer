726104e2abd7307507e3f04a54735b35408a86ec
Prince Rios <riosprince7@gmail.com>
2020-10-20 21:11:38 -0700

    Setup for test for emailAddressRetrieve endpoint with valid device id

--- apis_v1/tests/test_views_voter_email_address_retrieve.py
+++ apis_v1/tests/test_views_voter_email_address_retrieve.py
@@ -27,7 +27,20 @@ class WeVoteAPIsV1TestsVoterEmailAddressRetrieve(TestCase):
                          "status = {status} Expected status VALID_VOTER_DEVICE_ID_MISSING"
                          "voter_device_id: {voter_device_id}".format(status=json_data['status'],
                                                                      voter_device_id=json_data['voter_device_id']))
-        self.assertEqual(len(json_data["email_address_list"]), 1,
+        self.assertEqual(len(json_data["email_address_list"]), 0,
                          "Expected email_address_list to have length 0, "
                          "actual length = {length}".format(length=len(json_data['email_address_list'])))
 
+    def test_retrieve_with_voter_device_id(self):
+        response = self.client.get(self.generate_voter_device_id_url)
+        json_data = json.loads(response.content.decode())
+        voter_device_id = json_data['voter_device_id'] if 'voter_device_id' in json_data else ''
+
+        # Create a voter so we can test retrieve
+        response2 = self.client.get(self.voter_create_url, {'voter_device_id': voter_device_id})
+        json_data2 = json.loads(response2.content.decode())
+
+        self.assertEqual('status' in json_data2, True,
+                         "status expected in the voterEmailAddressRetrieveView json response but not found")
+        self.assertEqual('voter_device_id' in json_data2, True,
+                         "voter_device_id expected in the voterEmailAddressRetrieveView json response but not found")
