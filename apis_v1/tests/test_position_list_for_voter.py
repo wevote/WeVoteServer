d6ac6771e1460d26df13b92e994ba28e7e05ab47
Utsab Saha <utsab.k.saha@gmail.com>
2021-04-27 23:45:48 +0000

    Added test case for unregistered user

--- apis_v1/tests/test_position_list_for_voter.py
+++ apis_v1/tests/test_position_list_for_voter.py
@@ -19,7 +19,7 @@ class WeVoteAPIsV1TestsPositionListForVoter(TestCase):
         json_data = json.loads(response.content.decode())
         
         self.assertEqual('status' in json_data, True, "status expected in the json response, and not found")
-        self.assertEqual(json_data['status'],
+        self.assertEqual(json_data['status'].strip(),
                          "VALID_VOTER_DEVICE_ID_MISSING_VOTER_POSITION_LIST",
                          "status = {status} Expected status VALID_VOTER_DEVICE_ID_MISSING_VOTER_POSITION_LIST".format(status=json_data['status']))
         
@@ -30,3 +30,28 @@ class WeVoteAPIsV1TestsPositionListForVoter(TestCase):
         self.assertEqual(len(json_data["position_list"]), 0,
                          "Expected position_list to have length 0, "
                          "actual length = {length}".format(length=len(json_data['position_list'])))
+                         
+    
+    def test_retrieve_for_unregistered_user(self):
+        #Generate a voter_device_id
+        response = self.client.get(self.generate_voter_device_id_url)
+        json_data = json.loads(response.content.decode())
+        
+        self.assertEqual('voter_device_id' in json_data, True, "voter_device_id expected in the json response, and not found")
+        voter_device_id = json_data['voter_device_id']
+        
+        response2 = self.client.get(self.position_list_for_voter_retrieve_url, {'voter_device_id': voter_device_id})
+        json_data2 = json.loads(response2.content.decode())
+        
+        self.assertEqual(json_data2['status'].strip(),
+                         "VALID_VOTER_ID_MISSING_VOTER_POSITION_LIST",
+                         "status = {status} Expected status VALID_VOTER_ID_MISSING_VOTER_POSITION_LIST".format(status=json_data2['status']))
+        
+        self.assertEqual(json_data2['success'],
+                         False,
+                         "success = {success} Expected success".format(success=json_data2['success']))
+       
+        self.assertEqual(len(json_data2["position_list"]), 0,
+                         "Expected position_list to have length 0, "
+                         "actual length = {length}".format(length=len(json_data2['position_list'])))
+        
