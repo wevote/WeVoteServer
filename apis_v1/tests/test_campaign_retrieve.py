d1499234c44f28b5d0b492d1cddcc70abd982ffa
Luke Lyu <lukeforadream@gmail.com>
2021-05-30 06:51:17 +0000

    updated test filed with a utinity function

--- apis_v1/tests/test_campaign_retrieve.py
+++ apis_v1/tests/test_campaign_retrieve.py
@@ -14,4 +14,14 @@ class WeVoteAPIsV1TestsCampaignRetrieve(TestCase):
         self.generate_voter_device_id_url = reverse("apis_v1:deviceIdGenerateView")
         self.voter_create_url = reverse("apis_v1:voterCreateView")
         self.campaign_retrieve_url = reverse("apis_v1:campaignRetrieveView")
-    
+        
+#   campaignx = CampaignX.objects.create(
+#                     campaign_description=update_values['campaign_description'],
+#                     campaign_title=update_values['campaign_title'],
+#                     in_draft_mode=True,
+#                     started_by_voter_we_vote_id=voter_we_vote_id,
+#                     supporters_count=1,
+#                 )
+#                 campaignx_we_vote_id = campaignx.we_vote_id
+
+
