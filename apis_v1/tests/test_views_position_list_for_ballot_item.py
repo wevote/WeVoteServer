033af9736e3de985ba095ce4ff96ce564aecfd75
kmurata798 <kmurata798@gmail.com>
2021-06-27 17:02:37 -0700

    added test_retrieve_with_missing_ballot_item_id

--- apis_v1/tests/test_views_position_list_for_ballot_item.py
+++ apis_v1/tests/test_views_position_list_for_ballot_item.py
@@ -14,3 +14,59 @@ class WeVoteAPIsV1TestsPositionListForBallotItem(TestCase):
 
     def setUp(self):
         self.position_list_for_ballot_item_url = reverse("apis_v1:positionListForBallotItemView")
+
+    def test_retrieve_with_missing_ballot_item_id(self):
+        """
+        Test response when request has no ballot_item_id
+        """
+        response = self.client.get(self.position_list_for_ballot_item_url)
+        json_data = json.loads(response.content.decode())
+
+        # note: this status has no trailing whitespace, but others do
+        expected = {
+            "status": "POSITION_LIST_RETRIEVE_MISSING_BALLOT_ITEM_ID",
+            "success": False,
+            "count": 0,
+            "kind_of_ballot_item": "UNKNOWN",
+            "ballot_item_id": 0,
+            "ballot_item_we_vote_id": "",
+            "position_list": []
+        }
+
+        # status key should be in response
+        self.assertEqual('status' in json_data, True,
+                        "no 'status' returned in json response")
+
+        # "status": "POSITION_LIST_RETRIEVE_MISSING_BALLOT_ITEM_ID",
+        self.assertEqual(json_data['status'], expected['status'],
+                        f"Expected: {expected['status']}\n \
+                        Received: {json_data['status']} ")
+
+        # "success": False,
+        self.assertEqual(json_data['success'], expected['success'],
+                        f"Expected: {expected['success']}\n \
+                        Received: {json_data['success']} ")
+
+        # "count": 0,
+        self.assertEqual(json_data["count"], expected['count'],
+                        f"Expected: {expected['count']}\n \
+                        Received: {json_data['count']} ")
+
+        # "kind_of_ballot_item": "UNKNOWN",
+        self.assertEqual(json_data["kind_of_ballot_item"],
+                        expected['kind_of_ballot_item'],
+                        f"Expected: {expected['kind_of_ballot_item']}\nReceived: {json_data['kind_of_ballot_item']} ")
+
+        # "ballot_item_id": 0,
+        self.assertEqual(json_data["ballot_item_id"],
+                        expected['ballot_item_id'],
+                        f"Expected: {expected['ballot_item_id']}\nReceived: {json_data['ballot_item_id']} ")
+
+        # "ballot_item_we_vote_id": "",
+        self.assertEqual(json_data["ballot_item_we_vote_id"],
+                        expected['ballot_item_we_vote_id'],
+                        f"Expected: {expected['ballot_item_we_vote_id']}\nReceived: {json_data['ballot_item_we_vote_id']} ")
+
+        # "position_list": []
+        self.assertEqual(len(json_data["position_list"]), len(expected["position_list"]),
+                         f"Expected failed response to return empty position_list")
