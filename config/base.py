951417da637ef14e544e1fd458fb54a9292c315a
Jakaboy3 <75503857+Jakaboy3@users.noreply.github.com>
2022-06-20 18:20:07 +0000

    Commit

--- config/base.py
+++ config/base.py
@@ -11,7 +11,8 @@ import pathlib
 import re
 from django.core.exceptions import ImproperlyConfigured
 from django.db import connection
-
+import sys
+sys.path.append("/home/codespace/WeVoteServer3.5/WeVoteServer/lib/python3.8/site-packages/")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # Override in local.py for development
