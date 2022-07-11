c094ae3ae47912bd3432676ca43ca3c17eaacaff
stevepodell <steve@podell.com>
2022-07-11 15:39:29 -0700

    Catch an exception that is happening in retrieve_sql_tables_as_csv for the "FAST LOAD ALL OF THE ELECTION DATA, TO YOUR LOCAL POSTGRES" button

--- retrieve_tables/controllers.py
+++ retrieve_tables/controllers.py
@@ -82,30 +82,33 @@ def retrieve_sql_tables_as_csv(table_name, start, end):
 
         csv_files = {}
         if table_name in allowable_tables:
-            cur = conn.cursor()
-            csv_name = os.path.join(LOCAL_TMP_PATH, table_name + '.csvTemp')
-            print("exporting to: " + csv_name)
-            with open(csv_name, 'w') as file:
-                if positive_value_exists(end):
-                    sql = "COPY (SELECT * FROM public." + table_name + " WHERE id BETWEEN " + start + " AND " + end +\
-                          " ORDER BY id) TO STDOUT WITH DELIMITER '|' CSV HEADER NULL '\\N'"
-                else:
-                    sql = "COPY " + table_name + " TO STDOUT WITH DELIMITER '|' CSV HEADER NULL '\\N'"
-                cur.copy_expert(sql, file, size=8192)
-                logger.error("retrieve_tables sql: " + sql)
-            file.close()
-            with open(csv_name, 'r') as file2:
-                csv_files[table_name] = file2.read()
-            file2.close()
-            os.remove(csv_name)
-            if "exported" not in status:
-                status += "exported "
-            status += table_name + "(" + start + "," + end + "), "
-            conn.commit()
-            conn.close()
-            dt = time.time() - t0
-            logger.error('Extracting the "' + table_name + '" table took ' + "{:.3f}".format(dt) +
-                         ' seconds.  start = ' + start + ', end = ' + end)
+            try:
+                cur = conn.cursor()
+                csv_name = os.path.join(LOCAL_TMP_PATH, table_name + '.csvTemp')
+                print("exporting to: " + csv_name)
+                with open(csv_name, 'w') as file:
+                    if positive_value_exists(end):
+                        sql = "COPY (SELECT * FROM public." + table_name + " WHERE id BETWEEN " + start + " AND " + end +\
+                              " ORDER BY id) TO STDOUT WITH DELIMITER '|' CSV HEADER NULL '\\N'"
+                    else:
+                        sql = "COPY " + table_name + " TO STDOUT WITH DELIMITER '|' CSV HEADER NULL '\\N'"
+                    cur.copy_expert(sql, file, size=8192)
+                    logger.error("retrieve_tables sql: " + sql)
+                file.close()
+                with open(csv_name, 'r') as file2:
+                    csv_files[table_name] = file2.read()
+                file2.close()
+                os.remove(csv_name)
+                if "exported" not in status:
+                    status += "exported "
+                status += table_name + "(" + start + "," + end + "), "
+                conn.commit()
+                conn.close()
+                dt = time.time() - t0
+                logger.error('Extracting the "' + table_name + '" table took ' + "{:.3f}".format(dt) +
+                             ' seconds.  start = ' + start + ', end = ' + end)
+            except Exception as e:
+                logger.error("Real exception in retrieve_sql_tables_as_csv(): " + str(e) + " ")
         else:
             status = "the table_name '" + table_name + "' is not in the table list, therefore no table was returned"
             logger.error(status)
