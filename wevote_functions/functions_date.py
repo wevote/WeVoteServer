25fa3a56b638a8ce86f47e995b17da2558c485ce
Paul Jung <pbnjcub@gmail.com>
2024-05-02 10:17:21 -0400

    update/wv-315 Created new time functions

--- wevote_functions/functions_date.py
+++ wevote_functions/functions_date.py
@@ -2,6 +2,7 @@
 # Brought to you by We Vote. Be good.
 # -*- coding: UTF-8 -*-
 
+import pytz
 import datetime
 from wevote_functions.functions import positive_value_exists, convert_to_int, convert_to_str
 from math import log10
@@ -64,3 +65,27 @@ def convert_we_vote_date_string_to_date_as_integer(we_vote_date_string):
         return 0
 
 
+#new date constants and functions
+#new date format constants
+DATE_FORMAT_YMD_HMS = "%Y-%m-%d %H:%M:%S"                   #2024-03-04 21:58:40
+DATE_FORMAT_YMD_HM = "%Y-%m-%d %H:%M"                       #2024-03-04 21:58
+DATE_FORMAT_YMD_HM_SLASH = "%Y/%m/%d %H:%M"                 #2024/03/04 21:58
+DATE_FORMAT_YMD = "%Y-%m-%d"                                #2024-03-04
+DATE_FORMAT_YMD_SLASH = "%Y/%m/%d"                          #2024/03/04
+DATE_FORMAT_B_D_Y = "%b. %d, %Y"                            #Mar. 04, 2024
+DATE_FORMAT_YMDTHMSZ = "%Y-%m-%dT%H:%M:%S%z"                #2024-03-04T21:58:40Z
+DATE_FORMAT_A_D_B_Y_HMS_GMT = "%a, %d-%b-%Y %H:%M:%S GMT"   #Wed, 04-Mar-2024 21:58:40 GMT  
+DATE_FORMAT_MDY_IMS_P_SLASH = "%m/%d/%Y %I:%M:%S %p"        #03/04/2024 09:58:40 PM
+DATE_FORMAT_MDY_IMS_P = "%m/%d/%Y %H:%M"                    #03/04/2024 21:58
+DATE_FORMAT_BD_Y_AT_HM ="%B %d, %Y at %H:%M"                #March 04, 2024 at 21:58
+DATE_FORMAT_D = "%d"                                        #04
+
+
+# convert current date to a date as integer. replaces all instances when searching for "pytz.timezone"
+#     -import function into file
+#     -replace all instances when searching "pytz.timezone" with "date_today_as_integer = get_current_date_as_integer("America/Los_Angeles")"
+def get_current_date_as_integer(timezone_name="America/Los_Angeles"):
+    """Retrieve the current date as an integer formatted as YYYYMMDD in the specified timezone."""
+    timezone = pytz.timezone(timezone_name)
+    datetime_now = timezone.localize(datetime.now())
+    return convert_date_to_date_as_integer(datetime_now)
