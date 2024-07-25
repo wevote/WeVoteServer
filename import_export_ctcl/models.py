96fb371a46b24a6b3df23d5ca612d3763f5f0d30
Paolo Alberca <91231062+omgitsmiles@users.noreply.github.com>
2024-07-25 16:52:07 -0400

    removed self from methods

--- import_export_ctcl/models.py
+++ import_export_ctcl/models.py
@@ -54,7 +54,7 @@ class CTCLApiCounterManager(models.Manager):
 
     # WV-262 converting into static method
     @staticmethod
-    def create_counter_entry(self, kind_of_action, google_civic_election_id=0):
+    def create_counter_entry(kind_of_action, google_civic_election_id=0):
         """
         Create an entry that records that a call to the CTCL Api was made.
         """
@@ -79,7 +79,7 @@ class CTCLApiCounterManager(models.Manager):
         return results
 
     @staticmethod
-    def retrieve_daily_summaries(self, kind_of_action='', google_civic_election_id=0, days_to_display=30):
+    def retrieve_daily_summaries(kind_of_action='', google_civic_election_id=0, days_to_display=30):
         # Start with today and cycle backwards in time
         daily_summaries = []
         day_on_stage = date.today()  # TODO: We need to work out the timezone questions
