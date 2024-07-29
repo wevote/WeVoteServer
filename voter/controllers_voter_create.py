4f8a83defd8183b2fb6b8a89fd89b8e21f9c5652
ilona <gogiashvili.ilona@gmail.com>
2024-07-29 11:36:43 -0400

    [Issue #WV-261] Converted methods to @staticmethod

--- voter/controllers_voter_create.py
+++ voter/controllers_voter_create.py
@@ -2,13 +2,10 @@ from voter.models import VoterManager
 
 
 def create_dev_user():
-    first_name = "Samuel"
-    last_name = "Adams"
-    email = "samuel@adams.com"
-    password = "ale1776"
-    # To set up this user in the database:
-    # 1. Enter your information above.
-    # 2. Uncomment the "VoterManager().create_developer..." line below
-    # 3. Visit http://localhost:8000/voter/create_dev_user or https://wevotedeveloper.com:8000/voter/create_dev_user
-    # VoterManager().create_developer(first_name, last_name, email, password)
-    return
+    fName = "Ilona"
+    lName = "Gogiashvili"
+    email = "gogiashvili.ilona@gmail.com"
+    password = "sophia2018"
+    # Uncomment next line to set up this user in the database by visiting http://localhost:8000/voter/create_dev_user
+    VoterManager().create_developer(fName, lName, email, password)
+
